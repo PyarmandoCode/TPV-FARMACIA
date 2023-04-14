7d2a15d4882d57976e96606d02b0ec8172b1d792
hola.pyarmando.academy@gmail.com <hola.pyarmando.academy@gmail.com>
2023-04-14 15:18:40 -0500

    push

--- tpv_py/settings.py
+++ tpv_py/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ahpjj=av%(k2+!lv^_bpo0u&si#^t)8$+x4l^8%)32zkgaj%mf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
